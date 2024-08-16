c0cbf4fd7717ee11e2a68e53176def8a5d388ac2
KS <kyusang.dev@gmail.com>
2024-08-16 11:22:02 +0900

    Fix.SceneDelegate에서 Splash Coordinator 초기화 구현

--- RideThis/App/SceneDelegate.swift
+++ RideThis/App/SceneDelegate.swift
@@ -2,35 +2,16 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    var splashCoordinator: SplashCoordinator?
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         
-        let homeView = UINavigationController(rootViewController: HomeView())
-        homeView.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), selectedImage: nil)
-        
-        let competitionView = UINavigationController(rootViewController: CompetitionView())
-        competitionView.tabBarItem = UITabBarItem(title: "경쟁", image: UIImage(systemName: "flag.checkered.2.crossed"), selectedImage: nil)
-        
-        let recordView = UINavigationController(rootViewController: RecordView())
-        recordView.tabBarItem = UITabBarItem(title: "기록", image: UIImage(systemName: "flag.checkered"), selectedImage: nil)
-        
-        let deviceView = UINavigationController(rootViewController: DeviceView())
-        deviceView.tabBarItem = UITabBarItem(title: "장치연결", image: UIImage(systemName: "externaldrive.connected.to.line.below.fill"), selectedImage: nil)
-        
-        let myPageView = UINavigationController(rootViewController: MyPageView())
-        myPageView.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person.circle.fill"), selectedImage: nil)
-    
-        let tabbarController = UITabBarController()
-        // tabbarController.overrideUserInterfaceStyle = .light // 기본 라이트모드
-        tabbarController.viewControllers = [homeView, competitionView, recordView, deviceView, myPageView]
-        tabbarController.tabBar.tintColor = .primaryColor
-        
-        self.window?.rootViewController = tabbarController
-        self.window?.makeKeyAndVisible()
+        splashCoordinator = SplashCoordinator(window: self.window!)
+        splashCoordinator?.start()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {}
     func sceneDidBecomeActive(_ scene: UIScene) {}
     func sceneWillResignActive(_ scene: UIScene) {}
---
+++ SplashCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  SplashCoordinator.swift
+//  RideThis
+//
+//  Created by 황규상 on 8/16/24.
+//
+
+import Foundation
