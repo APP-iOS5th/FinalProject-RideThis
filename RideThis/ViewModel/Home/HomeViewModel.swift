85eb345b555712d220d1782c66c631053354b9da
KS <kyusang.dev@gmail.com>
2024-08-21 13:44:37 +0900

    Feat.Home 주간기록 viewModel 생성

--- RideThis/ViewModel/Home/HomeViewModel.swift
+++ RideThis/ViewModel/Home/HomeViewModel.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 class HomeViewModel {
+    struct WeeklyRecord {
+        let runCount: Int
+        let runTime: String
+        let runDistance: Double
+    }
     
+    let weeklyRecord: WeeklyRecord
+    
+    init() {
+        self.weeklyRecord = WeeklyRecord(runCount: 6, runTime: "15시간 34분", runDistance: 404.51)
+    }
 }
