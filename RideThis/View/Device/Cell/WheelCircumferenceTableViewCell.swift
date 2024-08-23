e287f26777037a7f201af6b2f7ad0ef8a88505a1
KS <kyusang.dev@gmail.com>
2024-08-23 18:35:32 +0900

    Feat.장치연결, 장치상세 화면에서 쓰이는 테이블 셀 추가

--- RideThis/View/Device/Cell/WheelCircumferenceTableViewCell.swift
+++ RideThis/View/Device/Cell/WheelCircumferenceTableViewCell.swift
@@ -1,8 +1,61 @@
-//
-//  WheelCircumferenceTableViewCell.swift
-//  RideThis
-//
-//  Created by 황규상 on 8/23/24.
-//
+import UIKit
+import SnapKit
 
-import Foundation
+class WheelCircumferenceTableViewCell: UITableViewCell {
+    static let identifier = "WheelCircumferenceTableViewCell"
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .right
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(valueLabel)
+        
+        containerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.height.greaterThanOrEqualTo(44)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(containerView).offset(16)
+            make.centerY.equalTo(containerView)
+        }
+        
+        valueLabel.snp.makeConstraints { make in
+            make.right.equalTo(containerView).offset(-16)
+            make.centerY.equalTo(containerView)
+            make.left.greaterThanOrEqualTo(titleLabel.snp.right).offset(8)
+        }
+    }
+    
+    func configure(title: String, value: String) {
+        titleLabel.text = title
+        valueLabel.text = value
+    }
+}
