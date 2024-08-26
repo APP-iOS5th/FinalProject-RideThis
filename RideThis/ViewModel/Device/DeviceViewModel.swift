1ef3871df5316e92a4dae62974ac412125adc453
KS <kyusang.dev@gmail.com>
2024-08-26 10:38:07 +0900

    Feat. 장치검색 데이터 추가 함수

--- RideThis/ViewModel/Device/DeviceViewModel.swift
+++ RideThis/ViewModel/Device/DeviceViewModel.swift
@@ -2,7 +2,9 @@ import Foundation
 import Combine
 
 class DeviceViewModel {
-    @Published private(set) var devices: [Device] = [
+    @Published private(set) var devices: [Device] = []
+    
+    @Published private(set) var searchedDevices: [Device] = [
         Device(name: "30832-1", serialNumber: "Serial 30832-1", firmwareVersion: "1.112", registrationStatus: "완료", wheelCircumference: "2.07m"),
         Device(name: "30832-2", serialNumber: "Serial 0832-2", firmwareVersion: "2.521", registrationStatus: "미완료", wheelCircumference: "4.08m"),
         Device(name: "30832-3", serialNumber: "Serial 0832-3", firmwareVersion: "3.164", registrationStatus: "미완료", wheelCircumference: "6.09m")
@@ -19,5 +21,10 @@ class DeviceViewModel {
         if selectedDevice?.name == deviceName {
             selectedDevice = nil
         }
+    }
+    
+    func addDevice(_ device: Device) {
+        guard !devices.contains(where: { $0.name == device.name }) else { return }
+        devices.append(device)
     }
 }
