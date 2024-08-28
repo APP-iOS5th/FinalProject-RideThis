86ade975110e2502e010cdc96d17ae22668c6128
SeongKookKIM <ksk8646@naver.com>
2024-08-28 13:45:28 +0900

    Fix.TabBar이름,competetion Result retry popToViewController, goalDistance

--- RideThis/View/Competition/ResultRanking/Coordinator/ResultRankingCoordinator.swift
+++ RideThis/View/Competition/ResultRanking/Coordinator/ResultRankingCoordinator.swift
@@ -30,9 +30,19 @@ class ResultRankingCoordinator: Coordinator {
     }
     
     func moveToRetry() {
-        let distanceCoordinator = DistanceSelectionCoordinator(navigationController: navigationController)
-         childCoordinators.append(distanceCoordinator)
+        // 네비게이션 스택에 뷰 컨트롤러가 있는지 확인
+        let viewControllers = navigationController.viewControllers
+        for controller in viewControllers {
+            if controller is DistanceSelectionViewController {
+                // DistanceSelectionViewController로 되돌아가기
+                navigationController.popToViewController(controller, animated: true)
+                return
+            }
+        }
         
-         distanceCoordinator.start()
+        // 만약 DistanceSelectionViewController가 스택에 없다면, 새로운 Coordinator로 시작
+        let distanceCoordinator = DistanceSelectionCoordinator(navigationController: navigationController)
+        childCoordinators.append(distanceCoordinator)
+        distanceCoordinator.start()
     }
 }
